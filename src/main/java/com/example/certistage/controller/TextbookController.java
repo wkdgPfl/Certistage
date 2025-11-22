73e5a23bc674314e5646e07131ed3fdc6ae192ca
wkdgPfl <bluebird1477@naver.com>
2025-11-22 15:08:22 +0900

    TextbookController 추가

--- src/main/java/com/example/certistage/controller/TextbookController.java
+++ src/main/java/com/example/certistage/controller/TextbookController.java
@@ -16,10 +16,11 @@ public class TextbookController {
 
         // 임시 더미 데이터
         return List.of(
-                new TextbookDto(1L, "정보처리기사 한 권으로 끝내기", 18000),
-                new TextbookDto(2L, "정보처리기사 기출문제집", 15000),
                 new TextbookDto(3L, "에듀윌 ADsP 책", 25000),
-                new TextbookDto(4L, "에듀윌 ADsP 책", 25000)
+                new TextbookDto(4L, "ADsP 합격을 위한 완벽 요약집", 27900),
+                new TextbookDto(5L, "이기적 SQLD 책 교재 2026", 21600),
+                new TextbookDto(6L, "내일은 SQL 개발자 SQLD 책 교재 시험합격", 21600)
+
         );
     }
 }
